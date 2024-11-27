8eb0645533ec3045683dc6ff3f6a09045f9cf710
Aniket Singh <aniketsingh90126@gmail.com>
2024-11-27 23:19:41 +0530

    feat: confirm delete modal and loader done

---
+++ src/Components/CustomModal.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types"; // Import prop-types for validation
+
+const CustomModal = ({ isOpen, onClose, title, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="CustomModal-overlay">
+      <div className="CustomModal-container">
+        <div className="CustomModal-header">
+          <h2>{title}</h2>
+          <button type="button" className="close-button" onClick={onClose}>
+            &times;
+          </button>
+        </div>
+        <div className="CustomModal-body">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+CustomModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
+
+export default CustomModal;
